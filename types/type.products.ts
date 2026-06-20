29bdb46a943b65268f602e9fa2eb12cd095984f4
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-20 10:23:35 +0545

    update file

---
+++ types/type.products.ts
@@ -0,0 +1,10 @@
+// type Products =
+//     {
+
+//         id: number;
+//         title: string;
+//         price: number;
+//         description: string;
+//         category: string;
+//         image: string
+//     }
