e2d245d79886d45c7177ce958cefba9bcb2db19c
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-20 10:23:10 +0545

    create product card section

---
+++ components/productcard.tsx
@@ -0,0 +1,20 @@
+// import React from "react";
+// import Image from "next/image";
+// export default function ProductCard({ data }: { data: Products }) {
+
+//     return (
+//         <>
+//             <h1>Product Pages</h1>
+//             <div>
+//                 <div>
+//                     <Image src={data.image} alt={"title"} width={500} height={300} unoptimized />
+//                     <h1>Title</h1>
+//                     <p>Price</p>
+//                 </div>
+//                 <div>Description</div>
+//                 <div>Category</div>
+//             </div>
+//         </>
+
+//     )
+// }
