0036462f61d7a72353d7fe4f4e5a0af367e30d46
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-22 09:20:36 +0545

    create next route page

---
+++ app/api/users/[id]/route.ts
@@ -0,0 +1,21 @@
+import { cookies } from "next/headers";
+import { NextRequest } from "next/server";
+
+
+export async function GET(req: NextRequest) {
+    const cookies_get = req.cookies.get("Hello friends")
+
+    const cookie = await cookies()
+    cookie.set("hello", "samiksha",)
+    return Response.json({ Message: "Hello World " });
+}
+
+
+export async function POST() {
+    return Response.json({ hello: "world" },
+        {
+            status: 200,
+        }
+
+    );
+}
