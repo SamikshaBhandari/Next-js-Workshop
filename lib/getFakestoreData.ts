5cde3aea081283bdb9a954ea1c5ea1b58b1fda87
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-23 20:57:41 +0545

    install npm i

--- lib/getFakestoreData.ts
+++ lib/getFakestoreData.ts
@@ -1,5 +1,5 @@
 export default async function getFakeStoreData(path: '/${string}') {
-    const res = await fetch('http://localhost:3000/api${path}', {
+    const res = await fetch('http://dev.tejbahadurkarki.name.np/api${path}', {
         next: {
             revalidate: 30,
             tags: ["api,path"]
