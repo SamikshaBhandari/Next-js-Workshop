3c7b3bd3d197c9677b5c17ba76cf41e20b65c49a
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-23 21:22:07 +0545

    refactor pages

--- components/productcard.tsx
+++ components/productcard.tsx
@@ -1,9 +1,16 @@
+import {
+    Card,
+    CardContent,
+    CardDescription,
+    CardFooter,
+    CardHeader,
+    CardTitle
+} from "@/components/ui/card";
 import Image from "next/image";
-import { Card, CardHeader } from "./ui/card";
 
-export default function ShadcnProcuctCard({ data }: { data: TypeProducts }) {
+export function ProcuctCard({ data }: { data: TypeProducts }) {
     return (
-        <Card className="max-w-md">
+        <div>
             <Image
                 src={data.image}
                 alt={data.title}
@@ -19,6 +26,30 @@ export default function ShadcnProcuctCard({ data }: { data: TypeProducts }) {
             <div>
                 <p>{data.description}</p>
             </div>
+        </div>
+    );
+}
+export default function ShadcnProcuctCard({ data }: { data: TypeProducts }) {
+    return (
+        <Card className="max-w-md">
+            <CardHeader className="h-52 overflow-hidden">
+                <Image
+                    src={data.image}
+                    alt={data.title}
+
+                    width={500}
+                    height={300}
+                    unoptimized
+                    className=" aspect-square object-cover"
+                />
+            </CardHeader>
+            <CardContent>
+                <CardTitle>{data.title}</CardTitle>
+                <p>{data.price}</p>
+            </CardContent>
+            <CardFooter>
+                <CardDescription>{data.description}</CardDescription>
+            </CardFooter>
         </Card>
     );
 }
