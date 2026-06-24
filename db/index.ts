96ccabb995bb400394a9f4b9f4f41282d3783805
Samikshya Bhandari <samikshyabhandari522@gmail.com>
2026-06-24 10:35:55 +0545

    update index.ts

--- db/index.ts
+++ db/index.ts
@@ -5,8 +5,6 @@ export const db = mysql2.createPool({
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
-    waitForConnections: true,
-    connectionLimit: 10,
-    queueLimit: 0,
+
 });
 
